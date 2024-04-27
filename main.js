1e771296f5af938ef70e3536c56e496b0c0350f5
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-27 20:38:06 +0500

    Function with array

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Make a array of magician’s names in a function
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
+}
+// Define array of magicians name
+let megicians_name = ["Usman", "Hamza", "Sunny", "Salman"];
+// Call the function to ptrint each function name
+show_magicians(megicians_name);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Make a array of magician’s names in a function
+function show_magicians(magicians:string[]){
+  magicians.forEach(name => console.log(name));
+}
+// Define array of magicians name
+let megicians_name = ["Usman","Hamza","Sunny","Salman"];
+// Call the function to ptrint each function name
+show_magicians(megicians_name)
